d0014c274d8e5e0353a89e937d1933dfafc4012b
konradPustelnik <konrad.pustelnik15@gmail.com>
2020-10-14 19:24:01 +0100

    Added style.css and overview of the page

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,3 +2,4 @@
 
 cd ../webServer/ && npm install express
 cd ../webServer/ && npm install ejs
+cd ../webServer/ && npm install multer
--- webServer/app.js
+++ webServer/app.js
@@ -8,4 +8,5 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', routes);
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(85);
