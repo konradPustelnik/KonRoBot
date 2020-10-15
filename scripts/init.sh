1dac0905eccbefbb7bd4ef5f8ed679b7ce269b77
konradPustelnik <konrad.pustelnik15@gmail.com>
2020-10-15 21:14:34 +0100

    Fix for init.sh and Makefile

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd ../webServer/ && npm install express
-cd ../webServer/ && npm install ejs
-cd ../webServer/ && npm install multer
+#install all packages for node js 
+cd ../webServer && npm install express && npm install ejs && npm install multer && cd -
+
+#pull-up resistors for buttons
+gpio -g mode 17 up
+gpio -g mode 26 up
+gpio -g mode 0 up
+
+#enable the control of light sensors
+sudo python3 readAnalogValues.py &
