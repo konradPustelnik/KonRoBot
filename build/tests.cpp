a42cfb1eb25321222796758facfddae53724256e
konradPustelnik <konrad.pustelnik15@gmail.com>
2020-11-12 17:23:54 +0000

    Added support for the TEST_F test cases.

--- build/tests.cpp
+++ build/tests.cpp
@@ -1,13 +1,42 @@
 #include <gtest/gtest.h>
 #include "../app/file.cpp"
 
-TEST(File, WriteReadFromFile) { 
+/*TEST(File, WriteReadFromFile) {
     File file("/tmp/tmpfile");
     ASSERT_EQ(true, file.write("test"));
-    ASSERT_EQ("test", file.read());
-    ASSERT_NE("failtest", file.read());
+    EXPECT_EQ("test", file.read());
+    EXPECT_NE("failtest", file.read());
 }
- 
+
+TEST(File, WriteReadFromOtherFile) {
+    File file("/tmp/tmpotherfile");
+    ASSERT_EQ(true, file.write("test"));
+    EXPECT_EQ("test", file.read());
+    EXPECT_NE("failtest", file.read());
+}*/
+
+class FileTest : public ::testing::Test {
+
+protected:
+    File createFile(std::string value) {
+        return File(value);
+    }
+};
+
+TEST_F(FileTest, WriteReadFromFile) {
+    auto file = createFile("/tmp/tmpfile");
+    ASSERT_EQ(true, file.write("test"));
+    EXPECT_EQ("test", file.read());
+    EXPECT_NE("failtest", file.read());
+}
+
+TEST_F(FileTest, WriteReadFromOtherFile) {
+    auto file = createFile("/tmp/tmpotherfile");
+    ASSERT_EQ(true, file.write("test"));
+    EXPECT_EQ("test", file.read());
+    EXPECT_NE("failtest", file.read());
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
