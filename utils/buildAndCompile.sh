723f89ac8ecf7968dd19b46da90ea17b963192d4
konradPustelnik <konrad.pustelnik15@gmail.com>
2021-01-21 18:05:09 +0000

    [tools]: add shellcheck support and minor fix for buildAndCompile.sh

--- utils/buildAndCompile.sh
+++ utils/buildAndCompile.sh
@@ -11,26 +11,24 @@ run_makefile()
 
 run_cmake()
 {
-    cmake -H. -Bbuild \
-        && cmake --build build
+    cmake -H. -Bbuild && cmake --build build
 }
 
 unit_tests_building()
 {
     (
-        cd tests \
-            && cmake -H. -Bbuild \
-                && cmake --build build \
-                    && ./build/runTests
+        cd tests || exit
+        cmake -H. -Bbuild \
+            && cmake --build build \
+                && ./build/runTests
     )
 }
 
 output()
 {
-    echo -e ""$(date +"%T":"%N")" ${BLUE}[$3]${NC} $1 Started" \
-        && $2 \
-            && echo -e ""$(date +"%T":"%N")" ${BLUE}[$4]${NC} $1 Completed" \
-                || { echo -e ""$(date +"%T":"%N")" ${RED}[$3] $1 Failed${NC}"; exit 1; }
+    echo -e "$(date +%T:%N) ${BLUE}[$3]${NC} $1 Started"
+    ($2 && echo -e "$(date +%T:%N) ${BLUE}[$4]${NC} $1 Completed") \
+        || { echo -e "$(date +%T:%N) ${RED}[$3] $1 Failed${NC}"; exit 1; }
 }
 
 output "Makefile" run_makefile "#     " "##    "
