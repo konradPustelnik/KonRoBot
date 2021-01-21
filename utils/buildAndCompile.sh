0312aedd2e392d75b3f66995c7d9e81d20e8d642
konradPustelnik <konrad.pustelnik15@gmail.com>
2021-01-21 17:45:55 +0000

    [code]: minor buildAndCompile.sh fix

--- utils/buildAndCompile.sh
+++ utils/buildAndCompile.sh
@@ -4,16 +4,6 @@ BLUE='\033[0;96m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-unit_tests_building()
-{
-    (
-        cd tests \
-            && cmake -H. -Bbuild \
-                && cmake --build build \
-                    && ./build/runTests
-    )
-}
-
 run_makefile()
 {
     make
@@ -25,6 +15,16 @@ run_cmake()
         && cmake --build build
 }
 
+unit_tests_building()
+{
+    (
+        cd tests \
+            && cmake -H. -Bbuild \
+                && cmake --build build \
+                    && ./build/runTests
+    )
+}
+
 output()
 {
     echo -e ""$(date +"%T":"%N")" ${BLUE}[$3]${NC} $1 Started" \
@@ -33,6 +33,6 @@ output()
                 || { echo -e ""$(date +"%T":"%N")" ${RED}[$3] $1 Failed${NC}"; exit 1; }
 }
 
-output "Unit Tests" unit_tests_building "#     " "##    "
-output "Makefile" run_makefile "###   " "####  "
-output "Cmake" run_cmake "##### " "######"
+output "Makefile" run_makefile "#     " "##    "
+output "Cmake" run_cmake "###   " "####  "
+output "Unit Tests" unit_tests_building "##### " "######"
